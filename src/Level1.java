4b5a83ba28cce3c46e2bfc397fcf6fdf984a40ef
kabirar95 <kabir.ramteke8@gmail.com>
2026-04-19 14:40:34 +0530

    ADDED WEEK1 CODE

---
+++ src/Level1.java
@@ -0,0 +1,28 @@
+//LEVEL1
+
+public class LEVEL1 {
+    public static void main(String[] args) {
+
+        // Q1
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+
+        // Q2
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam's average mark in PCM is " + average);
+
+        // Q3
+        double kilometers = 10.8;
+        double miles = kilometers / 1.6;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
